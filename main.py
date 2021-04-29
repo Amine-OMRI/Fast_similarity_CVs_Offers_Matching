3472135d49927accaf1fa30cd7cfb6499cefe10f
Amine OMRI <amine.omri@raccourci.fr>
2021-04-29 15:52:09 +0200

    Optimizing execution time and adding requirements.txt

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ import spacy
 import torch
 import pickle
 import config
+import time
+
 from typing import Optional
 from fastapi import FastAPI, HTTPException, Request
 app = FastAPI()
 
+nlp = spacy.load("fr_dep_news_trf", disable=['tagger', 'ner',
+                                             'morphologizer',
+                                             'attribute_ruler', 'lemmatizer'])
+
+
+embedder = SentenceTransformer(config.settings["bert_model"])
 
 @app.get("/api/similarity/{querie_text, requested_sample, exist_sample_embedds}")
 async def process_querie(querie_text: str = None, requested_sample: str = "problems",
-                   exist_sample_embedds: bool = False) -> list :
-    """
+                   exist_sample_embedds: Optional[bool] = False) -> list :
+    """embedder
     Load the comment and prepare it for the rest of processing
 
     Args:
@@ -25,7 +33,7 @@ async def process_querie(querie_text: str = None, requested_sample: str = "probl
     Return:
       dict: the cleaned dataset
     """
-
+    start_time = time.time()
     # Exceptions hundling
     if requested_sample not in config.samples.keys():
         raise HTTPException(
@@ -34,6 +42,8 @@ async def process_querie(querie_text: str = None, requested_sample: str = "probl
             headers={"X-Error": "There goes an error"},
         )
     results = await compute_similarity(querie_text, requested_sample, exist_sample_embedds)
+    print("--- Global time %s seconds ---" % (time.time() - start_time))
+
     return results
 
 async def compute_similarity(querie_text: str = None, requested_sample: str = "problems",
@@ -42,18 +52,28 @@ async def compute_similarity(querie_text: str = None, requested_sample: str = "p
     print("----------------------------------------------------------------------------------------------------------")
     print("[TEXT BEFORE PREPROCESSING]:", querie_text, "\n")
     # Clean the text
+
+    start_time = time.time()
     text = clean_text(querie_text)
+    print("--- Cleaning time time %s seconds ---" % (time.time() - start_time))
+
     # Remove emojis
+    start_time = time.time()
     text = remove_emoji(text)
+    print("--- Removing Emojis time %s seconds ---" % (time.time() - start_time))
+
     print("----------------------------------------------------------------------------------------------------------")
     print("[TEXT AFTER PREPROCESSING]:", text, "\n")
     # Split the text into multiple sub-texts
-    sub_texts = split_data(text)
+    start_time = time.time()
+    sub_texts = split_data(nlp, text)
+    print("--- Text Splitting time %s seconds ---" % (time.time() - start_time))
+
     print("----------------------------------------------------------------------------------------------------------")
     print("[TEXT AFTER SPLITTING]: TEXT :{}".format(len(sub_texts)))
-    for sent in sub_texts:
-        print("\t================================================")
-        print("\t" + sent)
+    # for sent in sub_texts:
+    #     print("\t================================================")
+    #     print("\t" + sent)
 
     # Loading the samples corpus
     samples_corpus = config.samples[requested_sample]
@@ -64,28 +84,43 @@ async def compute_similarity(querie_text: str = None, requested_sample: str = "p
     embedds_path = config.settings['sample_embedds_path'] + requested_sample + ".pickle"
     if exist_sample_embedds:
         # Loading the embeddings of the samples corpus
+        start_time = time.time()
         with open(embedds_path, 'rb') as input_file:
             sample_embedds = pickle.load(input_file)
             print("[INFO]: loading exsistant sample mebeddings ...\n")
+        print("--- Loading Embeddings time %s seconds ---" % (time.time() - start_time))
     else:
         # Creating Embeddings
-        sample_embedds = create_embeddings(samples_corpus)
+        start_time = time.time()
+        sample_embedds = create_embeddings(embedder, samples_corpus)
+        print("--- Calculating Embeddings time %s seconds ---" % (time.time() - start_time))
+
         # Save the calculated embeddings
         with open(embedds_path, 'wb') as output_file:
             pickle.dump(sample_embedds, output_file)
 
     # Calculate the embeddings of the querie text
-    querie_embedds = create_embeddings(sub_texts)
+    start_time = time.time()
+    querie_embedds = create_embeddings(embedder, sub_texts)
+    print("--- Calculating Querie Embeddings time %s seconds ---" % (time.time() - start_time))
+
     print("----------------------------------------------------------------------------------------------------------")
     print("[QUERIE EMBEDDINGS]:{}".format(querie_embedds.shape))
     print("[SAMPLES EMBEDDINGS]:{}".format(sample_embedds.shape))
 
     # Compute similarity
+    start_time = time.time()
     results = get_similarity(sample_embedds, querie_embedds)
+    print("--- Getting Similarity time %s seconds ---" % (time.time() - start_time))
+
     print("\n----------------------------------------------------------------------------------------------------------")
     print("[RESULTS BEFORE TUNING]:{}".format(results.shape))
 
+    # Tune results
+    start_time = time.time()
     results = tune_results(results, sub_texts)
+    print("--- Tuning Results time %s seconds ---" % (time.time() - start_time))
+
     print("----------------------------------------------------------------------------------------------------------")
     print("[RESULTS AFTER TUNING]:\n{}".format(results))
     return results
@@ -132,7 +167,7 @@ def remove_emoji(text: str = None) -> str:
     return text
 
 
-def split_data(text: str = None) -> list:
+def split_data(nlp, text: str = None) -> list:
     """
     Split the text using Spacy fr_dep_news_trf that is based on Camembert
     inorder to get shorter comments
@@ -143,12 +178,7 @@ def split_data(text: str = None) -> list:
         list[str]: la liste des fragments de text apré le découpage
     """
     split_contents = list()
-    comment_idx = list()
-    nlp = spacy.load("fr_dep_news_trf", disable=['tagger', 'ner',
-                                                 'morphologizer',
-                                                 'attribute_ruler', 'lemmatizer'])
     doc = nlp(text)
-    sub_sent = list()
 
     for sentence in doc.sents:
         split_contents.append(sentence.text)
@@ -164,7 +194,7 @@ def split_data(text: str = None) -> list:
     return list(data.Doc.values)
 
 
-def create_embeddings(text_list: list) -> torch.Tensor:
+def create_embeddings(embedder, text_list: list) -> torch.Tensor:
     """
     Create sentences embeddings
 
@@ -173,7 +203,7 @@ def create_embeddings(text_list: list) -> torch.Tensor:
     Return:
       dict: {"sentence":"label"} where label if 0 or 1 (1 = recommendation)
     """
-    embedder = SentenceTransformer(config.settings["bert_model"])
+    # embedder = SentenceTransformer(config.settings["bert_model"])
     # Query sentences:
     embeddings = embedder.encode(text_list)
     return embeddings
@@ -195,9 +225,9 @@ def get_similarity(queries_embedds: np.ndarray,
     # Find the closest sentences of the corpus for each query sentence based on cosine similarity
     # Threshold is the lowes similarity to be considered
     score = pd.DataFrame(cosine_similarity(samples_embedds, queries_embedds))
-    print("===>" + str(score.shape))
-    print(score)
-    print(score.index)
+    # print("===>" + str(score.shape))
+    # print(score)
+    # print(score.index)
 
     similarity = list()
     sample_index = list()
